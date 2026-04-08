6a5465766a4805b93bec86e4d46f216a3280b62f
Kshitij Shelake <129150233+Kshitij2005@users.noreply.github.com>
2026-04-08 11:05:18 +0530

    Refactor Shapes class with method and variable renaming
    Updated method names and constructor parameters for clarity and consistency.

--- Shapes.java
+++ Shapes.java
@@ -1,40 +1,43 @@
 class Shapes {
-    double radius, length, width, base, height;
+    double r, l, w, b, h;
 
-    Shapes(double radius) {
-        this.radius = radius;
+    // Constructor Overloading
+    Shapes(double r) {
+        this.r = r;
     }
 
-    Shapes(double length, double width) {
-        this.length = length;
-        this.width = width;
+    Shapes(double l, double w) {
+        this.l = l;
+        this.w = w;
     }
 
-    Shapes(double base, double height, boolean isTriangle) {
-        this.base = base;
-        this.height = height;
+    Shapes(double b, double h, char t) {
+        this.b = b;
+        this.h = h;
     }
 
-    double area() {
-        return 3.14 * radius * radius;
+    // Method Overloading
+    double findArea() { // Circle
+        return Math.PI * r * r;
     }
 
-    double area(double length, double width) {
-        return length * width;
+    double findArea(double l, double w) { // Rectangle
+        return l * w;
     }
 
-    double area(double base, double height, boolean isTriangle) {
-        return 0.5 * base * height;
+    double findArea(double b, double h, char t) { // Triangle
+        return 0.5 * b * h;
     }
 
     public static void main(String[] args) {
-        Shapes s1 = new Shapes(7);
-        System.out.println("Circle Area: " + s1.area());
 
-        Shapes s2 = new Shapes(4, 5);
-        System.out.println("Rectangle Area: " + s2.area(4, 5));
+        Shapes circle = new Shapes(7);
+        System.out.println("Circle Area: " + circle.findArea());
 
-        Shapes s3 = new Shapes(6, 8, true);
-        System.out.println("Triangle Area: " + s3.area(6, 8, true));
+        Shapes rectangle = new Shapes(4, 5);
+        System.out.println("Rectangle Area: " + rectangle.findArea(4, 5));
+
+        Shapes triangle = new Shapes(6, 8, 'T');
+        System.out.println("Triangle Area: " + triangle.findArea(6, 8, 'T'));
     }
 }
